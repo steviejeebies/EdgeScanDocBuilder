1f97892bd7e57141bb694387d15b0e220ede37fa
EmmetMcD <33671085+EmmetMcD@users.noreply.github.com>
2021-04-03 20:26:13 +0100

    Reverting
    Didn't mean to commit to main

--- app/html.js
+++ app/html.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 // Stephen: Commenting this part out just so we have to demo
 // version working. This is more-or-less a copy of Cian's PDF file
 
@@ -34,49 +33,39 @@
 // required to pass command line arguments from index.js
 module.exports = {
   docbuildHTML: docbuildHTML,
-  singleHTML: singleHTML,
 };
-
-// for full md-to-pdf config options see:
-// https://github.com/simonhaenisch/md-to-pdf/blob/master/src/lib/config.ts
-const outputOptions = {
-  // array of paths to stylesheets
-  // stylesheet:[path.resolve('./StyleSheets/Stylesheet2.css')],
-
-  // string of extra css properties
-  css: '',
-  as_html: true,
-
-  // extra options to pass to marked (the .md to .html renderer)
-  marked_options: {},
-
-  // options to be passed to puppeteer's pdf renderer
-  pdf_options: {
-    printBackground: true,
-    format: 'a4',
-    margin: {
-      top: '30mm',
-      right: '40mm',
-      bottom: '30mm',
-      left: '20mm',
-    },
-  },
-};
-
-function singleHTML(inputFile) {
-  const { mdToPdf } = require('md-to-pdf');
-  console.log('BEGINNING ' + inputFile + ' CONVERSION');
-  mdToPdf({ path: inputFile }, outputOptions)
-    .then(data => console.log('DATA: ' + data.content))
-    .then(data => { return data.content; })
-    .catch(console.error);
-}
 
 function docbuildHTML(argv) {
   const { mdToPdf } = require('md-to-pdf');
   const path = require('path');
   const fs = require('fs');
   const glob = require('glob');
+
+  // for full md-to-pdf config options see:
+  // https://github.com/simonhaenisch/md-to-pdf/blob/master/src/lib/config.ts
+  const outputOptions = {
+    // array of paths to stylesheets
+    // stylesheet:[path.resolve('./StyleSheets/Stylesheet2.css')],
+
+    // string of extra css properties
+    css: '',
+    as_html: true,
+
+    // extra options to pass to marked (the .md to .html renderer)
+    marked_options: {},
+
+    // options to be passed to puppeteer's pdf renderer
+    pdf_options: {
+      printBackground: true,
+      format: 'a4',
+      margin: {
+        top: '30mm',
+        right: '40mm',
+        bottom: '30mm',
+        left: '20mm',
+      },
+    },
+  };
 
   let inputDir = argv.source;
   let outputDir = argv.html_destination;
@@ -87,10 +76,10 @@ function docbuildHTML(argv) {
 
   let targetFiles = glob.sync(`${inputDir}/**/*.md`);
 
-  // // FIXME: literally no idea why this won't work forwards
-  // // Stephen: I removed .reverse(), but `docbuild --html` needs to be called
-  // // in the sample_documents folder (i.e the directory that contains
-  // // the docs/ folder)
+  // FIXME: literally no idea why this won't work forwards
+  // Stephen: I removed .reverse(), but `docbuild --html` needs to be called
+  // in the sample_documents folder (i.e the directory that contains
+  // the docs/ folder)
   targetFiles.reverse().forEach(inputFile => {
     // same name as the input file, and places it in the `build` directory
     let baseName = path.basename(inputFile, path.extname(inputFile));
