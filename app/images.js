d518ccf4fe3f5199bcbd1654eea8894b32f85ce9
Stephen Rowe <47434040+steviejeebies@users.noreply.github.com>
2021-04-20 21:34:20 +0100

    Update images.js

--- app/images.js
+++ app/images.js
@@ -11,7 +11,7 @@ const glob = require('glob');
 const imgbbUploader = require('imgbb-uploader');
 
 // this is just for a throwaway account, 0 private
-// info on it, doesn't matter if token is public on GitHub
+// info on it, doesn't matter if token is visible on GitHub
 const imgBBToken = '3d083719235d5d9fb11ec9cf902fb954';
 
 let fileTypes = 'gif,jpeg,jpg,tiff,png,bmp,GIF,JPEG,JPG,TIFF,PNG,BMP';
